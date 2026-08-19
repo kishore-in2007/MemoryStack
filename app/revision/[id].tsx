e7521c2240ae02d1b26d320831cd2f53313d4eee
kishore-in2007 <kishoresakthikumar839@gmail.com>
2026-08-19 20:14:38 +0530

    fix duplicate revision route startup crash

--- app/revision/[id].tsx
+++
@@ -1,1 +0,0 @@
-import {Redirect,useLocalSearchParams} from 'expo-router';export default function LegacyRevision(){const {id}=useLocalSearchParams<{id:string}>();return <Redirect href={`/revision/${id}/review`}/>}
